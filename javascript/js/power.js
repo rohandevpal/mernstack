640c0099181e3a908bee2e09dcbc5d723cd4fd57
rachna <rachna7956gitHub>
2022-10-13 18:36:48 +0530

    four

---
+++ javascript/js/power.js
@@ -0,0 +1,20 @@
+// find power of a number
+
+const inputOne = document.getElementById('one');
+const inputTwo = document.getElementById('two');
+const res = document.querySelector('#res');
+
+
+function fucn() {
+    var first = document.getElementById("one").value;
+    var second = document.getElementById("two").value;
+    var result = document.getElementById('result').textContent = Math.pow(first, second);
+    // document.write(Math.pow(first, second))
+
+    
+}
+
+// res.addEventListener('click', ()=>{
+//     console.log(x);
+// })
+
