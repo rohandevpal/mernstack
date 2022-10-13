59bcb341075ba2fdf29423d107fd8fc7dd48311f
rachna <rachna7956gitHub>
2022-10-13 18:06:45 +0530

    third

--- javascript/js/calculator.js
+++ javascript/js/calculator.js
@@ -1,6 +1,8 @@
 const value = document.querySelectorAll('.cal');
 const input = document.querySelector('.one');
 const equele = document.querySelector('#equals');
+const AC = document.querySelector('#AC');
+
 
 let cal = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/"];
 let countarr = cal.length;
@@ -14,14 +16,17 @@ for (i = 0; i < 14; i++) {
 function myFunction(ele) {
     var id = ele.id;
     input.value += id;
-
+       
 }
 
 equele.addEventListener('click', () => {
     let final = eval(document.querySelector('.one').value);
     document.getElementById('res').value = final;
+})
 
-
+AC.addEventListener('click', () => {
+    input.value = "";
+    document.getElementById('res').value = "";
 })
 
 
